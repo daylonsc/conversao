b29a1201af6f20cd6fb8c89c310fdfb7fccd769e
Daylon Costa <daylon.costa@e-deploy.com.br>
2020-12-28 19:06:02 -0300

    criacao do projeto

---
+++ src/main/java/com/jaya/test/conversao/ConversaoApplication.java
@@ -0,0 +1,13 @@
+package com.jaya.test.conversao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConversaoApplication.class, args);
+    }
+
+}
